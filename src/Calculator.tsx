2455a60f8a718eb5ad8945dd7335511a65f41335
WebiWaboK <pepin21112003@gmail.com>
2024-09-06 20:10:49 -0600

    Si

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import Button from './Button';
+import './Calculator.css';
 
 function Calculator() {
-    const [displayValue, setDisplayValue] = useState('0');
+    const [displayValue, setDisplayValue] = useState<string>('0');
     const [operator, setOperator] = useState<String | null > (null);
     const [firstOperand, setFirstOperand] = useState<number | null>(null)
 
@@ -47,13 +48,41 @@ function Calculator() {
         }
     }
 
+    function handleClearClick() {
+        setDisplayValue('0');
+        setFirstOperand(null);
+        setOperator(null);
+    }
+
 
     return (
-        <div>
-            <div>{displayValue}</div>
-
-                <Button value="1" onClick={() => handleNumberClick('1')}></Button>{' '}
-                <Button value="+" onClick={() => handleOperationClick('+')}></Button>{' '}
+        <div className="calculator">
+            {' '}
+            <div className="display">{displayValue}</div> {' '}
+            <div>
+                <button value="7" onClick={() => handleNumberClick('7')}></button>
+                <button value="4" onClick={() => handleNumberClick('4')}></button>
+                <button value="1" onClick={() => handleNumberClick('1')}></button>
+                <button value="0" onClick={() => handleNumberClick('0')}></button>
+            </div>
+            <div>
+                <button value="8" onClick={() => handleNumberClick('8')}></button>
+                <button value="5" onClick={() => handleNumberClick('5')}></button>
+                <button value="2" onClick={() => handleNumberClick('2')}></button>
+                <button value="=" onClick={calculate}></button>
+            </div>
+            <div>
+                <button value="9" onClick={() => handleNumberClick('9')}></button>
+                <button value="6" onClick={() => handleNumberClick('6')}></button>
+                <button value="3" onClick={() => handleNumberClick('3')}></button>
+                <button value="-" onClick={() => handleNumberClick('-')}></button>
+            </div>
+            <div>
+                <button value="C" onClick={handleClearClick}></button>
+                <button value="/" onClick={() => handleOperationClick('/')}></button>
+                <button value="*" onClick={() => handleOperationClick('*')}></button>
+                <button value="+" onClick={() => handleOperationClick('+')}></button>
+            </div>
         </div>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -6,5 +6,5 @@ import Calculator from './Calculator';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Calculator />
-  </React.StrictMode>,
+  </React.StrictMode>, //si
 )
